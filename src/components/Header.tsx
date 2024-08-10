ede715d4121654c0afd6b8c9692c6f808e99ecf7
Felixx-jnr <uyuoukoh@gmail.com>
2024-08-10 02:26:22 +0100

    fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,4 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import { Container } from "./Container";
-import { Button } from "./Button";
 
 export const Header = () => {
   return (
