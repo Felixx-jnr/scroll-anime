9d5036d17e25e6722928a356ba91832381695382
Felixx-jnr <uyuoukoh@gmail.com>
2026-08-03 13:52:34 +0100

    Update videoCarousel.tsx

--- src/components/section/videoCarousel.tsx
+++ src/components/section/videoCarousel.tsx
@@ -77,7 +77,7 @@ export const VideoCarousel = () => {
                 muted
               >
                 <source
-                  src="https://res.cloudinary.com/dyf0wsiaf/video/upload/v1724065774/ghbjnxrjh5ly2gx1qccr.mp4"
+                  src="https://calvaryhillglobalchurch.s3.eu-north-1.amazonaws.com/Johnnie/Whisky+Johnnie+Walker+Red+Label+commercial+(1080p).mp4"
                   type="video/mp4"
                 />
                 Your browser does not support the video tag.
@@ -94,7 +94,7 @@ export const VideoCarousel = () => {
                 muted
               >
                 <source
-                  src="https://res.cloudinary.com/dyf0wsiaf/video/upload/v1724065734/hi60ttwmpqjp1afhtuxy.mp4"
+                  src="https://calvaryhillglobalchurch.s3.eu-north-1.amazonaws.com/Johnnie/Johnnie+Walker+Black+Label+(1080p).mp4"
                   type="video/mp4"
                 />
                 Your browser does not support the video tag.
@@ -120,7 +120,7 @@ export const VideoCarousel = () => {
                 muted
               >
                 <source
-                  src="https://res.cloudinary.com/dyf0wsiaf/video/upload/v1724065798/onuegyzptver27oa9rdk.mp4"
+                  src="https://calvaryhillglobalchurch.s3.eu-north-1.amazonaws.com/Johnnie/Johnnie+Walker+Black+Cask+Commercial+(2026)+(1080p60fps).mp4"
                   type="video/mp4"
                 />
                 Your browser does not support the video tag.
