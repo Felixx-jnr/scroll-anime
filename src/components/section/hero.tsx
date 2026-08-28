8ceae4517d0168de04cc79f4d4d697e6b673a1a6
Felixx-jnr <uyuoukoh@gmail.com>
2026-08-28 10:04:59 +0100

    Update video source URL in hero component

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -25,7 +25,7 @@ export const Hero = () => {
           muted
         >
           <source
-            src="https://calvaryhillglobalchurch.s3.eu-north-1.amazonaws.com/Johnnie/Johnnie+Walker++-+CINEMATIC+WHISKY+COMMERCIAL+(1080p).mp4"
+            src="https://felixx-projects.s3.eu-north-1.amazonaws.com/Johnnie/Johnnie+Walker++-+CINEMATIC+WHISKY+COMMERCIAL+(1080p).mp4"
             type="video/mp4"
           />
           Your browser does not support the video tag.
